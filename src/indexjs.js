bafcc111cfd4882e526701dd7da48a80292ef4d5
shivam kumar <shivamkumar@cedcoss.com>
2022-02-05 16:38:00 +0530

    intriducing else if

--- src/indexjs.js
+++ src/indexjs.js
@@ -435,6 +435,21 @@ function testEqual(val) {
   }
   
   testElse(4);
+
+  function testElseIf(val) {
+    if (val > 10) {
+      return "Greater than 10";
+    }
+  
+    else if (val < 5) {
+      return "Smaller than 5";
+    }
+  else{
+    return "Between 5 and 10";
+  }
+  }
+  
+  testElseIf(7);
   
 
 
