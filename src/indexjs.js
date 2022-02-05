d3c4267615644b20b704ab74462f022bd9abb395
shivam kumar <shivamkumar@cedcoss.com>
2022-02-05 11:27:15 +0530

    subtract two no by js

--- src/indexjs.js
+++ src/indexjs.js
@@ -2,4 +2,5 @@ const FCC = "freeCodeCamp"; // Change this line
 let fact = "is cool!"; // Change this line
 fact = "is awesome!";
 console.log(FCC, fact); // Change this line
-const sum = 10 + 10;s
+const sum = 10 + 10;
+const difference = 45 - 33;
