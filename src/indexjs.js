75b880d50ddc39ce2f1df3e6d8467737f2a5ec5b
shivam kumar <shivamkumar@cedcoss.com>
2022-02-05 15:40:22 +0530

    Stand in Line

--- src/indexjs.js
+++ src/indexjs.js
@@ -250,3 +250,21 @@ function processArg(num) {
 
 // Only change code below this line
 processed = processArg(7)
+
+
+function nextInLine(arr, item) {
+    // Only change code below this line
+    arr.push(item);
+    var removed = arr.shift();
+    return removed;
+    
+    // Only change code above this line
+  }
+  
+  // Setup
+  const testArr = [1, 2, 3, 4, 5];
+  
+  // Display code
+  console.log("Before: " + JSON.stringify(testArr));
+  console.log(nextInLine(testArr, 6));
+  console.log("After: " + JSON.stringify(testArr));
