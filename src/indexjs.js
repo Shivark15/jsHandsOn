1caea37c50e907b7cecb3d773c8e95d2607fc336
shivam kumar <shivamkumar@cedcoss.com>
2022-02-05 13:58:04 +0530

    pop a multi dimensional array data

--- src/indexjs.js
+++ src/indexjs.js
@@ -143,4 +143,8 @@ const myArray = [18, 64, 99];
   // Only change code below this line
   
   myArray.push(["dog", 3]);
-   
+
+  const myArray = [["John", 23], ["cat", 2]];
+
+// Only change code below this line
+ const removedFromMyArray = myArray.pop() ;
