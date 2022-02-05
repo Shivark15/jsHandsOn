bd0e280d36bc7bec0895fc14ae50a7488b1e3689
shivam kumar <shivamkumar@cedcoss.com>
2022-02-05 15:58:06 +0530

    practice if statement using strict equality operator

--- src/indexjs.js
+++ src/indexjs.js
@@ -305,3 +305,12 @@ function testEqual(val) {
   }
   
   testStrict(10);
+
+  function compareEquality(a, b) {
+    if (a === b) { // Change this line
+      return "Equal";
+    }
+    return "Not Equal";
+  }
+  
+  compareEquality(10, "10");
