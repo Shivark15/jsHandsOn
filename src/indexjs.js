b3b6f1d32e53af5329991a0972ce9cf1e1dc7e98
shivam kumar <shivamkumar@cedcoss.com>
2022-02-05 11:08:13 +0530

    Understanding Uninitialized Variables

--- src/indexjs.js
+++ src/indexjs.js
@@ -1,9 +1,9 @@
-// Setup
-var a = 9;
-
-var b;
-b = a ;
-var myFirstName = "shiva";
-var myLastName = "kumar" ;
-
 // Only change code below this line
+var a = 5 ;
+var b = 10 ; 
+var c = "I am a";
+// Only change code above this line
+
+a = a + 1;
+b = b + 5;
+c = c + " String!";
