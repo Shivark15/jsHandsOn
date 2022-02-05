28fd4e35d19082867c0c9c55bc9b0c0b0c6fb179
shivam kumar <shivamkumar@cedcoss.com>
2022-02-05 15:32:24 +0530

    ssignment with a Returned Value

--- src/indexjs.js
+++ src/indexjs.js
@@ -240,3 +240,13 @@ function addFive(){
 
 addThree();
 addFive();
+
+// Setup
+let processed = 0;
+
+function processArg(num) {
+  return (num + 3) / 5;
+}
+
+// Only change code below this line
+processed = processArg(7)
