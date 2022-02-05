24949e0173182908500fbfbd59613f8aa33ec839
shivam kumar <shivamkumar@cedcoss.com>
2022-02-05 16:01:25 +0530

    not equal operatorr

--- src/indexjs.js
+++ src/indexjs.js
@@ -314,3 +314,12 @@ function testEqual(val) {
   }
   
   compareEquality(10, "10");
+
+  function testNotEqual(val) {
+    if (val!=99) { // Change this line
+      return "Not Equal";
+    }
+    return "Equal";
+  }
+  
+  testNotEqual(10);
