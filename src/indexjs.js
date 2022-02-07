c6a5965943cef4b2aa9129d7407420ddd6103745
shivam kumar <shivamkumar@cedcoss.com>
2022-02-07 09:55:35 +0530

    boolean

--- src/indexjs.js
+++ src/indexjs.js
@@ -617,7 +617,14 @@ function caseInSwitch(val) {
   
   chainToSwitch(7);
 
-
+  function isLess(a, b) {
+    // Only change code below this line
+    return (a <= b) 
+      
+    // Only change code above this line
+  }
+  
+  isLess(10, 15);
 
 
 
